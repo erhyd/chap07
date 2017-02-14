24942b6854c9948a7361cd36e058185d9d9b4c15
java <java>
2017-02-14 10:44:22 +0900

    third commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
